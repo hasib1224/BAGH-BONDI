280e4c7c0dc43aefaf7f658eb30ed94e73b47db0
Hasib Abdullah Sarker <99174217+hasib1224@users.noreply.github.com>
2022-04-28 16:15:15 +0600

    separated header file

---
+++ TwoPlayerMode.h
@@ -0,0 +1,70 @@
+#ifndef twoPlayer_h
+#include"goat.h"
+#include"WinningScreen.h"
+#include"menu.h"
+
+void two_player_mode(){
+   
+   cleardevice();
+   board_view();
+
+   while(1){
+      tiger_suc_input=0;
+      goat_suc_input=0;
+
+      while(1){                 //.....goat input loop.......
+         if(total_goat>0){
+            goat_input(); 
+
+            if(goat_suc_input==5){
+              cleardevice();
+              board_view();
+              break;
+            }
+
+         }else if(total_goat==0){
+            goat_scope();
+
+            if(goat_suc_input==5){
+              cleardevice();
+              board_view();
+              break;
+            }
+
+         }
+
+      }
+
+
+      if( goat_win_flag!=0 ){ 
+         //.....Tiger input loop........
+         while(1){
+            tiger_scope(); 
+            if(tiger_suc_input==6) break;
+         
+         } 
+
+      }
+
+
+      if(killed_goat==5){      //.....TIGER WINNING SCREEEN.......
+        tiger_win_screen();
+        break;
+      }
+
+      if(goat_win_flag==0){   //.....GOAT WINNING SCREEN..........
+        goat_win_screen();
+        break;
+
+      }
+
+
+   }   
+
+}
+
+#define twoPlayer_h
+#endif
+
+
+
